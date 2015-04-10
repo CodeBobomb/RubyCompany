c9f58c60b74540dd7e71e312302bfa8f5333456e
bjusufbe <bakir@atlantbh.com>
2015-04-10 13:46:00 +0200

    small refactoring of spec

--- test/spec/company/objects_finding_spec.rb
+++ test/spec/company/objects_finding_spec.rb
@@ -1,64 +1,63 @@
 require_relative "../../helpers/company_creator"
 
+describe "finding employees/teams/projects in company object" do 
 
-describe " finding employees/teams/projects in company object " do 
-before do
-	@company=CompanyCreator.create_company
-end
-context "finding employee by id " do
-context "existing id "
-it  "id: 1 " do
-	emp=@company.find_employee(1)
-	expect(emp.id).to eql(1)
-end
-end
-context "nonexisting id: "
-it "id: 123 " do
-	emp=@company.find_employee(123)
-	expect(emp.nil?).to be_truthy
-end
-end
-context "finding team by team name " do
-context "existing team " do
-it "team name: AndroidDevTeam "
-	team=@company.find_team_by_name("AndroidDevTeam")
-	expect(team.team_name).to eql("AndroidDevTeam")
-end
-end
-context "nonexisting team " do
-it "team: AtlantTeam"
-	team=@company.find_team_by_name("AtlantTeam")
-	expect(team.nil?).to be_truthy
-end
-end
-end
-context "finding team by project name " do
-context "existing project " do
-it "project: Android "
-	team=@company.find_team_by_project("Android")
-	expect(team.project_name).to eql("Android")
-end
-end
-context "nonexisting project " do
-it "project: .NET"
-	team=@company.find_team_by_project(".NET")
-	expect(team.nil?).to be_truthy
-end
-end
-context "finding active projects of a member " do
-context "member of a team "
-it "id: 1" do
-	teams=@company.member_of_teams(1)
-	expect(teams[0].has_member?(1)).to be_truthy
-end
-end
-context "not a team member "
-before :all do
-	@company.add_employee(Developer.new("John","Doe",444))
-end
-it "id: 123" do
-	teams=@company.member_of_teams(444)
-	expect(teams[0].nil?).to be_truthy
-end
-end
+	let(:company) { @company=CompanyCreator.create_company }
+
+	context "Find employee by existing id: 1" do
+		it  "found employee" do
+			emp=company.find_employee(1)
+			expect(emp.id).to eql(1)
+		end
+	end
+
+	context "Find employee by non-existing id: 123" do
+		it "did not found employee" do
+			emp=company.find_employee(123)
+			expect(emp.nil?).to be_truthy
+		end
+	end
+
+	context "Find team by existing team: AndroidDevTeam" do
+		it "found team" do
+			team=company.find_team_by_name("AndroidDevTeam")
+			expect(team.team_name).to eql("AndroidDevTeam")
+		end
+	end
+
+	context "Find team by non-existing team: AtlantTeam" do
+		it "did not found team" do
+			team=company.find_team_by_name("AtlantTeam")
+			expect(team.nil?).to be_truthy
+		end
+	end
+
+	context "Find team by existing project: Android" do
+		it "found project" do
+			team=company.find_team_by_project("Android")
+			expect(team.project_name).to eql("Android")
+		end
+	end
+
+	context "Find team by non-existing project: .NET" do
+		it "did not found project" do
+			team=company.find_team_by_project(".NET")
+			expect(team.nil?).to be_truthy
+		end
+	end
+
+	context "Find active projects for team member with id: 1" do
+		it "found projects" do
+			teams=company.member_of_teams(1)
+			expect(teams[0].has_member?(1)).to be_truthy
+		end
+	end
+
+	context "Find active projects of member that is not part of the team: (John Doe)" do
+		it "did not found active projects" do
+			company.add_employee(Developer.new("John","Doe",444))
+			teams=company.member_of_teams(444)
+			expect(teams[0].nil?).to be_truthy
+		end
+	end
 end
