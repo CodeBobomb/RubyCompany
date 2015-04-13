ecf14b114e6073e84dcb7791e5f56eabd38531f0
bjusufbe <bakir@atlantbh.com>
2015-04-13 20:49:55 +0200

    adding comments for potential fixes

--- GitScript.sh
+++ GitScript.sh
@@ -6,3 +6,13 @@ sudo chmod -R 777 ~/RubyCompany/
 cd ~/RubyCompany/test/config/
 ruby build_increment.rb
 cd ~/RubyCompany/test
+
+# Nikad ne koristiti sudo u bash skriptama. Ako ti je potreban sudo access, mozes samu skriptu pokrenuti sa sudo
+# ali za ovo ti ne bi trebao biti potreban
+
+# Napravi malo vise logike ovdje u smislu da provjeris da li postoji RubyCompany folder, ako postoji obrises ga,
+# ako ne, onda i ne pokusavas rm komandu
+
+# Zasto dodajes all permisije na RubyCompany?
+
+# Nema komande za pokretanje testova?
