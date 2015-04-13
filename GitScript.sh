4208c78a2f4f09841131b7a08fb705e04719c0aa
CodeBobomb <muslija.adnan@gmail.com>
2015-04-13 16:06:27 +0200

    fixed script

--- GitScript.sh
+++ GitScript.sh
@@ -2,5 +2,7 @@
 sudo rm -rf ~/RubyCompany
 sudo git clone https://github.com/CodeBobomb/RubyCompany.git
 sudo mv RubyCompany ~/
+sudo chmod -R 777 ~/RubyCompany/
+cd ~/RubyCompany/test/config/
+ruby build_increment.rb
 cd ~/RubyCompany/test
-rspec spec/*
