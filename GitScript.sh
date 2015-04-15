bcc5157944dff5bcddcfad14736a50efee6ff09b
CodeBobomb <muslija.adnan@gmail.com>
2015-04-15 19:43:08 +0200

    script fixed vs

--- GitScript.sh
+++ GitScript.sh
@@ -19,20 +19,29 @@ function build_inc {
 	fi
 
 
-	s=" build: \"${ver[0]}.${ver[1]}.${ver[2]}\""
+	s="  build: \"${ver[0]}.${ver[1]}.${ver[2]}\""
 	sed -i "s/.*build.*/$s/" ~/RubyCompany/test/config/rspec2db.yml
 }
 
 
-if [ -d  ~/"RubyCompany"  ] && [ -d ~/"RubyCompany/.git" ]; then
+if [ -d  ~/"RubyCompany"  ] && [ -d ~/"RubyCompany/.git" ]; 
+	then
 	echo "Trying to pull the latest build"
 	cd ~/RubyCompany
 	output=$(git pull)
-	if [ !$(output)==$(Already up-to-date.) ] then
-		build_inc
+	check="Already up-to-date."
+	if [ "$output"=="$check" ];
+	then
+		echo "No changes on remote"
+	else
+		echo "Pulled the latest commit"
 	fi
 else
 	echo "cloning RubyCompany"
 	git clone https://github.com/CodeBobomb/RubyCompany.git ~/RubyCompany
 fi
+build_inc
 
+cd ~/RubyCompany/test
+
+rspec ~/RubyCompany/test/spec/*
